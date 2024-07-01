b1176dd0ac8ba1b5aea1eca9c1787a0e37730acb
Dhruv Kelawala <dhruvrk2000@gmail.com>
2024-07-01 12:50:32 +0530

    feat: add copy functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-const cameraPermissionButton = document.getElementById(
-  "cameraPermissionbutton"
-);
+const mainContainer = document.getElementById("mainContainer");
 
 window.onload = async () => {
+  const cameraPermissionButton = document.getElementById(
+    "cameraPermissionbutton"
+  );
+
   try {
     const permission = await navigator.permissions.query({ name: "camera" });
 
@@ -12,7 +14,7 @@ window.onload = async () => {
     } else {
       const template = document.getElementById("cameraPermissionTemplate");
       const clone = document.importNode(template.content, true);
-      document.getElementById("mainContainer").appendChild(clone);
+      mainContainer.appendChild(clone);
 
       cameraPermissionButton.addEventListener("click", async () => {
         try {
@@ -39,7 +41,7 @@ window.onload = async () => {
 function onCameraPermissionGranted(stream) {
   const template = document.getElementById("scanQRTemplate");
   const clone = document.importNode(template.content, true);
-  document.getElementById("mainContainer").appendChild(clone);
+  mainContainer.appendChild(clone);
 
   const video = document.getElementById("cameraView");
   const canvasElement = document.getElementById("canvas");
@@ -63,26 +65,62 @@ function tick(video, canvasElement, canvas) {
     );
     const code = jsQR(imageData.data, imageData.width, imageData.height);
     if (code) {
-      processData(code.data);
+      const txData = processData(code.data);
       // Stop the video stream after finding a QR code
       video.srcObject.getTracks().forEach(track => track.stop());
+
+      showTxInfo(txData);
     }
   }
   requestAnimationFrame(() => tick(video, canvasElement, canvas));
 }
 
-async function processData(data) {
+function processData(data) {
+  // decode the base64 data
   const decodedData = Base64.toUint8Array(data);
-  console.log("🚀 ~ processData ~ decodedData:", decodedData);
+  // inflate the data
   const inflatedData = fflate.inflateSync(decodedData);
-  console.log("🚀 ~ processData ~ inflatedData:", inflatedData);
-  const textDecoder = new TextDecoder();
-  const tx = textDecoder.decode(inflatedData);
-  console.log("🚀 ~ processData ~ unparsed tx:", tx);
-
+  // decode the inflated data
+  const tx = new TextDecoder().decode(inflatedData);
+  // parse the tx and calculate the hash
   const parsedTx = json.parse(tx);
-  console.log("🚀 ~ processData ~ parsed tx:", parsedTx);
-
   const txHash = calculateInvokeTransactionHash(parsedTx);
-  console.log("🚀 ~ processData ~ txHash:", txHash);
+  return { tx, txHash };
+}
+
+function showTxInfo(txData) {
+  const { tx, txHash } = txData;
+  const prettifiedTx = jsonFormatter.format(tx, "\t");
+  const template = document.getElementById("airGappedData");
+  const clone = document.importNode(template.content, true);
+
+  // Tx Hash
+  clone.querySelector("#tx-hash-content").textContent = txHash;
+
+  // Raw Data
+  const pre = document.createElement("pre");
+  pre.textContent = prettifiedTx;
+  pre.className = "tx-data";
+  clone.querySelector("#raw-data-content").appendChild(pre);
+
+  mainContainer.innerHTML = "";
+  mainContainer.appendChild(clone);
+
+  const copyTxHashButton = document.getElementById("copyTxHash");
+  copyTxHashButton.addEventListener("click", () => {
+    copyToClipboard(txHash);
+  });
+
+  const copyRawDataButton = document.getElementById("copyRawData");
+  copyRawDataButton.addEventListener("click", () => {
+    copyToClipboard(tx);
+  });
+}
+
+async function copyToClipboard(text) {
+  try {
+    await navigator.clipboard.writeText(text);
+  } catch (error) {
+    console.error("Copy failed", error);
+  }
 }
